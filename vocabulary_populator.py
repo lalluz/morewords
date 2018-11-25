9152add11cb0a8d8351dd58f598eab4c1f22b949
lara zilio <larazilio@gmail.com>
2018-11-25 15:07:59 +0100

    Update vocabulary populator

--- vocabulary_populator.py
+++ vocabulary_populator.py
@@ -2,28 +2,28 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from morewords import Base, User, Language, Word
-#from flask.ext.sqlalchemy import SQLAlchemy
 
 
 engine = create_engine('sqlite:///morewords.db')
-
 Base.metadata.bind = engine
-
 DBSession = sessionmaker(bind=engine)
-
 session = DBSession()
+
 
 # Add Users
 def add_users():
-    user_1 = User(username="user_1", email="user_1@example.com")
+    user_1 = User(username="user_1",
+                  email="user_1@example.com")
     user_1.hash_password("password_1")
     session.add(user_1)
 
-    user_2 = User(username="user_2", email="user_2@example.com")
+    user_2 = User(username="user_2",
+                  email="user_2@example.com")
     user_2.hash_password("password_2")
     session.add(user_2)
 
-    user_3 = User(username="user_3", email="user_3@example.com")
+    user_3 = User(username="user_3",
+                  email="user_3@example.com")
     user_3.hash_password("password_3")
     session.add(user_3)
 
@@ -31,79 +31,189 @@ def add_users():
 
     return
 
+
 # Add Languages
 def add_languages():
-    italian = Language(name="italian", user_id=1)
+    italian = Language(name="italian",
+                       user_id=1)
     session.add(italian)
 
-    spanish = Language(name="spanish", user_id=2)
+    spanish = Language(name="spanish",
+                       user_id=2)
     session.add(spanish)
 
-    french = Language(name="french", user_id=3)
+    french = Language(name="french",
+                      user_id=3)
     session.add(french)
 
-    german = Language(name="german", user_id=1)
+    german = Language(name="german",
+                      user_id=1)
     session.add(german)
 
     session.commit()
 
     return
 
+
 # Add Words
 def add_words():
     # Add Italian Words
-    cane = Word(name="cane", translation="dog", notes="some notes here", is_learned=False, language_id=1, user_id=1)
+    cane = Word(name="cane",
+                translation="dog",
+                notes="some notes here",
+                is_learned=False,
+                language_id=1,
+                user_id=1)
     session.add(cane)
-    gatto = Word(name="gatto", translation="cat", notes="", is_learned=False, language_id=1, user_id=2)
+
+    gatto = Word(name="gatto",
+                 translation="cat",
+                 is_learned=False,
+                 language_id=1,
+                 user_id=2)
     session.add(gatto)
-    mucca = Word(name="mucca", translation="cow", notes="some notes here", is_learned=False, language_id=1, user_id=2)
+
+    mucca = Word(name="mucca",
+                 translation="cow",
+                 notes="some notes here",
+                 language_id=1,
+                 user_id=2)
     session.add(mucca)
-    pettirosso = Word(name="pettirosso", translation="robin", notes="", is_learned=False, language_id=1, user_id=1)
+
+    pettirosso = Word(name="pettirosso",
+                      translation="robin",
+                      language_id=1,
+                      user_id=1)
     session.add(pettirosso)
-    merlo = Word(name="merlo", translation="blackbird", notes="", is_learned=True, language_id=1, user_id=1)
+
+    merlo = Word(name="merlo",
+                 translation="blackbird",
+                 is_learned=True,
+                 language_id=1,
+                 user_id=1)
     session.add(merlo)
-    gufo = Word(name="gufo", translation="owl", notes="some notes here", is_learned=True, language_id=1, user_id=1)
+
+    gufo = Word(name="gufo",
+                translation="owl",
+                notes="some notes here",
+                is_learned=True,
+                language_id=1,
+                user_id=1)
     session.add(gufo)
-    volpe = Word(name="volpe", translation="fox", notes="", is_learned=False, language_id=1, user_id=2)
+
+    volpe = Word(name="volpe",
+                 translation="fox",
+                 language_id=1,
+                 user_id=2)
     session.add(volpe)
-    pinguino = Word(name="pinguino", translation="penguin", notes="", is_learned=False, language_id=1, user_id=1)
+
+    pinguino = Word(name="pinguino",
+                    translation="penguin",
+                    language_id=1,
+                    user_id=1)
     session.add(pinguino)
-    libro_it = Word(name="libro", translation="book", notes="some notes here", is_learned=False, language_id=1, user_id=1)
+
+    libro_it = Word(name="libro",
+                    translation="book",
+                    notes="some notes here",
+                    language_id=1,
+                    user_id=1)
     session.add(libro_it)
-    casa_it = Word(name="casa", translation="house", notes="", is_learned=True, language_id=1, user_id=1)
+
+    casa_it = Word(name="casa",
+                   translation="house",
+                   is_learned=True,
+                   language_id=1,
+                   user_id=1)
     session.add(casa_it)
 
     # Add Spanish Words
-    perro = Word(name="perro", translation="dog", notes="", is_learned=False, language_id=2, user_id=1)
+    perro = Word(name="perro",
+                 translation="dog",
+                 language_id=2,
+                 user_id=1)
     session.add(perro)
-    libro_es = Word(name="libro", translation="book", notes="", is_learned=False, language_id=2, user_id=3)
+
+    libro_es = Word(name="libro",
+                    translation="book",
+                    language_id=2,
+                    user_id=3)
     session.add(libro_es)
-    casa_es = Word(name="casa", translation="house", notes="some notes here", is_learned=True, language_id=2, user_id=2)
+
+    casa_es = Word(name="casa",
+                   translation="house",
+                   notes="some notes here",
+                   is_learned=True,
+                   language_id=2,
+                   user_id=2)
     session.add(casa_es)
-    mesa = Word(name="mesa", translation="table", notes="", is_learned=True, language_id=2, user_id=2)
+
+    mesa = Word(name="mesa",
+                translation="table",
+                is_learned=True,
+                language_id=2,
+                user_id=2)
     session.add(mesa)
-    hola = Word(name="hola", translation="hello", notes="", is_learned=True, language_id=2, user_id=1)
+
+    hola = Word(name="hola",
+                translation="hello",
+                is_learned=True,
+                language_id=2,
+                user_id=1)
     session.add(hola)
-    arroz = Word(name="arroz", translation="rice", notes="", is_learned=False, language_id=2, user_id=3)
+
+    arroz = Word(name="arroz",
+                 translation="rice",
+                 language_id=2,
+                 user_id=3)
     session.add(arroz)
-    agua = Word(name="agua", translation="water", notes="some notes here", is_learned=False, language_id=2, user_id=2)
+
+    agua = Word(name="agua",
+                translation="water",
+                notes="some notes here",
+                language_id=2,
+                user_id=2)
     session.add(agua)
-    silla = Word(name="silla", translation="chair", notes="", is_learned=False, language_id=2, user_id=2)
+
+    silla = Word(name="silla",
+                 translation="chair",
+                 language_id=2,
+                 user_id=2)
     session.add(silla)
 
     # Add French Words
-    oui = Word(name="oui", translation="yes", notes="some notes here", is_learned=True, language_id=3, user_id=3)
+    oui = Word(name="oui",
+               translation="yes",
+               notes="some notes here",
+               is_learned=True,
+               language_id=3,
+               user_id=3)
     session.add(oui)
-    deux = Word(name="deux", translation="two", notes="", is_learned=False, language_id=3, user_id=3)
+
+    deux = Word(name="deux",
+                translation="two",
+                language_id=3,
+                user_id=3)
     session.add(deux)
-    soleil = Word(name="soleil", translation="sun", notes="some notes here", is_learned=True, language_id=3, user_id=3)
+
+    soleil = Word(name="soleil",
+                  translation="sun",
+                  notes="some notes here",
+                  language_id=3,
+                  user_id=3)
     session.add(soleil)
-    maison = Word(name="maison", translation="house", notes="", is_learned=True, language_id=3, user_id=3)
+
+    maison = Word(name="maison",
+                  translation="house",
+                  is_learned=True,
+                  language_id=3,
+                  user_id=3)
     session.add(maison)
 
     session.commit()
 
     return
+
 
 add_users()
 add_languages()
