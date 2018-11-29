22ea5ddca243014b44dd7aa428dbebf6c2d1672a
lara zilio <larazilio@gmail.com>
2018-11-29 16:07:16 +0100

    Fix indentation

--- morewords.py
+++ morewords.py
@@ -469,7 +469,7 @@ def gconnect():
 
     # Verify that the access token is valid for this app.
     CLIENT_ID = json.loads(
-    open('client_secrets.json', 'r').read())['web']['client_id']
+        open('client_secrets.json', 'r').read())['web']['client_id']
     if result['issued_to'] != CLIENT_ID:
         response = (make_response(
                     json.dumps("Token's client ID does not match app's."),
